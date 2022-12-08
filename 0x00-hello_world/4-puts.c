8c16c89dcfa25e2cf0de06ebb1a4a4d537a0a07f
Essie-wanjiru001 <esthernjeriwanjiru287@gmail.com>
2022-12-08 13:00:58 -0800

    task completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - main block
+ * puts - output
+ * Return: 0
+ * */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
